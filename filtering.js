f5643f32ea9c5c767135c5fe3ce5126b83f326bc
Julie K <julieskim160@gmail.com>
2018-05-16 12:20:45 -0700

    filtering an array

---
+++ filtering.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const arr = [2, 4, 6, 9]
+
+function filterArray(array) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] >=5) {
+            newArr.push(array[i]);
+        }
+    }
+    return newArr;
+}
+
+console.log(filterArray(arr));
