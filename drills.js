8655f2b73b4c7e71d4edb3f7a390a14d487084a0
Julie K <julieskim160@gmail.com>
2018-05-16 08:35:16 -0700

    push

--- drills.js
+++ drills.js
@@ -1,4 +1,20 @@
 'use strict';
 
-const MemoryInfo = require('./memory');
-let Memory = MemoryInfo();
+const memory = require('./memory');
+
+
+class juArray {
+    constructor() {
+        this.length = 0;
+        this.ptr = memory.allocate(this.length);
+    }
+
+    push(value) {
+        if(this.length >= this._capacity) {
+            this._resize((this.length +1) * Array.SIZE_RATIO);
+        }
+        memory.set(this.ptr + this.length, value);
+        this.length++;
+    }
+
+}
