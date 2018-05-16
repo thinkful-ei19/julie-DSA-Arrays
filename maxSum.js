25d7418118070a8d53b88ca52cdac92321d3d4a6
Julie K <julieskim160@gmail.com>
2018-05-16 12:42:56 -0700

    max sum in the array

---
+++ maxSum.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const example = [4,6, -3, 5, -2, 1]
+
+function maxSum(arr) {
+    let sum = 0;
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        if( sum > max) {
+            max = sum;
+        }
+    }
+
+    return max;
+}
+
+console.log(maxSum(example));
