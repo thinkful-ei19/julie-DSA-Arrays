f7c9c9f5c102ef9d052ae0784bb178c215e10df6
Julie K <julieskim160@gmail.com>
2018-05-16 19:45:14 -0700

    2d 1/2

--- 2d.js
+++ 2d.js
@@ -16,6 +16,22 @@
 // [0,0,0,0,0],
 // [0,0,1,1,0]];
 
-function twoD() {
+function twoD(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
+            newArray.push(0)
+        }
+        for (let j = 0; j < array.length; j++) {
 
+        }
+    }
 }
+
+//going through 2 sets of arrays
+//grid
+//for loop is going through x coord
+//based on the i and j value, x, y coordinate and update
+//nested for loop 
+
+//O(n^2) - Polynomial Time 
